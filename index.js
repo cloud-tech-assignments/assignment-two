d305f817c088aa7659dc3080988d2cd38cb4cd61
Sandmal <sandmal.cornelius@gmail.com>
2021-05-06 21:11:09 +0200

    Save json to database

--- index.js
+++ index.js
@@ -1,28 +1,61 @@
 const mosca = require("mosca");
 const settings = { port: 1883 };
 const broker = new mosca.Server(settings);
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const PlantModel = require("./plants.model");
+const { input } = require("./config").config;
 
-broker.on("ready", () => {
-  console.log("Broker is ready!");
-});
+// Used to convert exi and xml payload to json
+const EXI4JSON = require("exificient.js");
+const parser = require("xml2json");
 
 broker.on("published", async (packet) => {
-  const payload = packet.payload.toString();
-  if (payload.slice(0, 1) == "{" && !payload.includes("client")) {
-    const plant = new PlantModel({ payload });
-    await plant.save();
+  let payload = packet.payload.toString();
+  const array = payload.split(",");
+  let output;
+
+  switch (input) {
+    case "json":
+      output = "{";
+      break;
+    case "xml":
+      output = "<";
+      break;
+    case "exi":
+      output = "1";
+      break;
+  }
+  if (payload.slice(0, 1) == output && !payload.includes("client")) {
+    if (input == "xml") {
+      payload = JSON.stringify(parser.toJson(payload));
+    }
+
+    if (input == "exi") {
+      payload = JSON.stringify(EXI4JSON.parse(array));
+    }
+
+    const plantData = new PlantModel({ payload });
+    await plantData.save();
+    console.log(
+      `Payload [ ${input.toUpperCase()} ] is now saved as JSON in db`
+    );
   }
 });
 
-// // MongoDB
-// mongoose
-//   .connect("mongodb://localhost:27017/plants", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//   })
-//   .then(() => console.log("Connected to DB!"))
-//   .catch((error) => console.log(error));
+// When broker is started up we console log that its up and running
+const startup = () => {
+  console.log("Broker is up and running");
+};
+
+broker.on("ready", startup);
+
+// MongoDB
+mongoose
+  .connect("mongodb://localhost:27017/plants", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Connected to DB!"))
+  .catch((error) => console.log(error));
