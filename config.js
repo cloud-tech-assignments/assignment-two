2dc431a76b2e9b9dd04f64440c8b8706f755b6a6
Sandmal <sandmal.cornelius@gmail.com>
2021-05-06 21:10:58 +0200

    Added things to config

--- config.js
+++ config.js
@@ -5,13 +5,24 @@ const mqtt = require("mqtt");
 exports.config = {
   client: mqtt.connect("mqtt://localhost:1883"),
   input: "xml", // json, xml and exi for corresponding output
-  topic: "PLANTS",
-  string: {
+  numOfPublishers: 2, // type 1 for one pubslisher and 2 for two publishers
+  topicOne: "PLANTS/sensorOne", // topic for subscriber one
+  topicTwo: "PLANTS/sensorTwo", // topic for subscriber two
+  sensorOne: {
     data: {
       plant: {
-        name: "Holy",
-        temperature: Math.floor(Math.random() * 20), // creates random temperature values
-        humidity: Math.floor(Math.random() * 20), // creates random humidity values
+        id: 1,
+        type: "ESP8266",
+        temperature: Math.floor(Math.random() * 20),
+      },
+    },
+  },
+  sensorTwo: {
+    data: {
+      plant: {
+        id: 2,
+        type: "ESP8266",
+        humidity: Math.floor(Math.random() * 20),
       },
     },
   },
